5d78dd1a72ff40d20aa199534d0ecf5f512d6905
Mateusz Perlak <147995089+UczenAGH@users.noreply.github.com>
2024-01-07 16:21:47 +0100

    Update main.py
    zmiana w main - apple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys, random
+import Apple
 from pygame.math import Vector2
 
 pg.init()
@@ -9,14 +10,7 @@ screen = pg.display.set_mode((width, height))
 FPS = 60
 clock = pg.time.Clock()
 
-class Apple:
-    def __init__(self):
-        self.x = 40 * random.randint(0, (width / 40) - 1)
-        self.y = 40 * random.randint(0, (height / 40) - 1 )
-        self.pos = Vector2(self.x, self.y)
-    def draw(self):
-        sprite = pg.Rect(self.pos.x, self.pos.y, 40, 40)
-        pg.draw.rect(screen, (126,0,0), sprite)
+
 
 class Snake:
     def __init__(self):
